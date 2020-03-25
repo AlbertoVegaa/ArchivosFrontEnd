a163d1d30b5e2f8eb81996cf48e624bd58e9c0ce
Alberto Vega <JesusVega14@outlook.es>
2020-03-25 16:06:25 -0500

    Firebase Auth

---
+++ Autenticacion/js/autenticacion.js
@@ -0,0 +1,21 @@
+window.onload = inicializar;
+var formAutenticacion;
+
+function inicializar(){
+    formAutenticacion = document.getElementById("form-autenticacion");
+    formAutenticacion.addEventListener("submit", autentificar, false);
+}
+
+function autentificar(event){
+    event.preventDefault();
+    var usuario = event.target.email.value;
+    var contrasena = event.target.password.value;
+
+    firebase.auth().signInWithEmailAndPassword(usuario, contrasena)
+    .then(function(result){
+        window.location.href = "prueba1.html";
+    })
+    .catch(function(error) {
+        $("#errorModal").modal();
+      });
+}
---
+++ Autenticacion/js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCChTlB7gD-N9RgY2helvb0g3zj1nWrIFc",
+    authDomain: "emprendete-842ea.firebaseapp.com",
+    databaseURL: "https://emprendete-842ea.firebaseio.com",
+    projectId: "emprendete-842ea",
+    storageBucket: "emprendete-842ea.appspot.com",
+    messagingSenderId: "781556060139",
+    appId: "1:781556060139:web:0d083661053fe4dd8053ae",
+    measurementId: "G-2K5SBKQM8V"
+};
+
+firebase.initializeApp(firebaseConfig);
