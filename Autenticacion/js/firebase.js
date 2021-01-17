326759e81796e379cb6aa2c79f225a29d267492f
Alberto Vega <JesusVega14@outlook.es>
2021-01-17 12:05:35 -0600

    Nose

--- Autenticacion/js/firebase.js
+++ Autenticacion/js/firebase.js
@@ -1,12 +1,21 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyA-qsu_Q98hfrnTwT7118KllwVzAuLuBSA",
+//     authDomain: "restaurantesmatamoros.firebaseapp.com",
+//     databaseURL: "https://restaurantesmatamoros.firebaseio.com",
+//     projectId: "restaurantesmatamoros",
+//     storageBucket: "restaurantesmatamoros.appspot.com",
+//     messagingSenderId: "383927893628",
+//     appId: "1:383927893628:web:d87130685a8b9f13e053f9",
+//     measurementId: "G-326WDF5SX3"
+//   };
 var firebaseConfig = {
-    apiKey: "AIzaSyA-qsu_Q98hfrnTwT7118KllwVzAuLuBSA",
-    authDomain: "restaurantesmatamoros.firebaseapp.com",
-    databaseURL: "https://restaurantesmatamoros.firebaseio.com",
-    projectId: "restaurantesmatamoros",
-    storageBucket: "restaurantesmatamoros.appspot.com",
-    messagingSenderId: "383927893628",
-    appId: "1:383927893628:web:d87130685a8b9f13e053f9",
-    measurementId: "G-326WDF5SX3"
-  };
+      apiKey: "AIzaSyAolZ3RTeKa9ypeD9iouQy9sB1DzrYMy0A",
+      authDomain: "subir-y-ver.firebaseapp.com",
+      databaseURL: "https://subir-y-ver.firebaseio.com",
+      projectId: "subir-y-ver",
+      storageBucket: "subir-y-ver.appspot.com",
+      messagingSenderId: "154329800121",
+      appId: "1:154329800121:web:bedbf3189a09dfbb216a68"
+};
 
   firebase.initializeApp(firebaseConfig);
