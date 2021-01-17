77f6bca7e54eb402c578287a94e84d13e0ce2fc4
Alberto Vega <JesusVega14@outlook.es>
2021-01-17 14:37:48 -0600

    Update firebase.js

--- Autenticacion/js/firebase.js
+++ Autenticacion/js/firebase.js
@@ -1,21 +1,11 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyA-qsu_Q98hfrnTwT7118KllwVzAuLuBSA",
-//     authDomain: "restaurantesmatamoros.firebaseapp.com",
-//     databaseURL: "https://restaurantesmatamoros.firebaseio.com",
-//     projectId: "restaurantesmatamoros",
-//     storageBucket: "restaurantesmatamoros.appspot.com",
-//     messagingSenderId: "383927893628",
-//     appId: "1:383927893628:web:d87130685a8b9f13e053f9",
-//     measurementId: "G-326WDF5SX3"
-//   };
 var firebaseConfig = {
-      apiKey: "AIzaSyAolZ3RTeKa9ypeD9iouQy9sB1DzrYMy0A",
-      authDomain: "subir-y-ver.firebaseapp.com",
-      databaseURL: "https://subir-y-ver.firebaseio.com",
-      projectId: "subir-y-ver",
-      storageBucket: "subir-y-ver.appspot.com",
-      messagingSenderId: "154329800121",
-      appId: "1:154329800121:web:bedbf3189a09dfbb216a68"
+  apiKey: "AIzaSyAbLWaxW7pIsuhbqfWDKUlvQ_VglM5Y73Y",
+  authDomain: "rotarymatamoros.firebaseapp.com",
+  projectId: "rotarymatamoros",
+  storageBucket: "rotarymatamoros.appspot.com",
+  messagingSenderId: "89212099931",
+  appId: "1:89212099931:web:61f6336cc1f109244a4e7e",
+  measurementId: "G-WEJS8GJPXE"
 };
-
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
